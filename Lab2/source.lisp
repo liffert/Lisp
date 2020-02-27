487aa4655492fd98b8031a3366027215b285748f
liffert <digitizenetwork@gmail.com>
2020-02-27 15:13:51 +0200

    complete lab 2

--- Lab2/source.lisp
+++ Lab2/source.lisp
@@ -1,5 +1,11 @@
+(defun is_exist (lst)
+  (print "Item is exist")
+  lst)
+
 (defun associative-list-add (lst key value)
-  (append lst (list (cons key value))))
+  (if (null (car (associative-list-get lst key)))
+      (append lst (list (cons key value)))
+      (is_exist lst)))
 
 (defun associative-list-get (lst key)
   (cond
@@ -9,36 +15,73 @@
 
 
 (defun property-list-add (lst key value)
-  (append lst (list key value)))
+  (if (null (car (PROPERTY-LIST-GET lst key)))
+      (append lst (list key value))
+      (is_exist lst)))
 
 (defun property-list-get (lst key )
   (cond
     ((null lst) (list nil nil))
     ((eq (car lst) key) (list (car (cdr lst)) t))
-    (t (property-list-get (cdr (cdr lst)) key))
-    ))
+    (t (property-list-get (cdr (cdr lst)) key))))
 
-(defun binary-tree-add (tree key value))
 
-(defun binary-tree-get (tree key))
+(defun binary-tree-add (tree key value)
+  (let ((Left (cadr tree))
+	(Right (caddr tree))
+	(entry (car tree)))
+    (cond
+      ((null tree) (list (cons key value) nil nil))
+      ((string= key (car entry)) (is_exist tree))
+      ((string< key (car entry)) (list entry (binary-tree-add Left key value) Right))
+      ((string> key (car entry)) (list entry Left (binary-tree-add right  key value)))
+      )))
+(defun binary-tree-get (tree key)
+  (let ((Left (cadr tree))
+	(Right (caddr tree))
+	(entry (car tree)))
+    (cond
+      ((null tree) (list nil nil))
+      ((string= key (car entry)) (list (cdr entry) t))
+      ((string< key (car entry)) (binary-tree-get Left key))
+      ((string> key (car entry)) (binary-tree-get right key))
+      )))
+
 
 (defun main ()
+ 
 
-  (let ((lst '()))
-    (setf lst (property-list-add lst :a 1))
-    (setf lst (property-list-add lst :b 2))
-    (print lst)
-    (print (PROPERTY-LIST-GET lst :c))
-    (print (property-list-get lst :b)))
+  (let ((lst '())
+	(tree '())
+	(lstp '()))
 
-  (let ((lst '()))
+    (print "Properti list")
+    (setf lstp (property-list-add lstp :a 1))
+    (setf lstp (property-list-add lstp :b 2))
+    (print lstp)
+
+    (print (PROPERTY-LIST-GET lstp :c))
+    (print (property-list-get lstp :b))
+
+    (print "Associative list")
     (setf lst (associative-list-add lst :a 1))
-    (print lst)
     (setf lst (associative-list-add lst :b 23))
     (setf lst (associative-list-add lst :c 2))
     (print lst)
+    
     (print (associative-list-get lst :b))
     (print (associative-list-get lst :c))
-    (print (associative-list-get lst :s)))
+    (print (associative-list-get lst :s))
+
+    (print "binary tree")
+    (setf tree (binary-tree-add tree "a" 1))
+    (setf tree (binary-tree-add tree "b" 2))
+    (setf tree (binary-tree-add tree "c" 3))
+    (setf tree (binary-tree-add tree "aaa" 4))
+    (print tree)
+
+    (print (binary-tree-get tree "c"))
+    (print (binary-tree-get tree "s")))
+    
   '("End"))
 
